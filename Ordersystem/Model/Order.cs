e21c9d42ee4ae74dbcb1b95092cdb33a2db3b425
Monzy <Magnushansen12@hotmail.com>
2015-11-16 15:24:46 +0100

    Add Order class

---
+++ Ordersystem/Model/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Order
+    {
+        public List<Serving> Servings;
+
+        public void AddServing(Dish dish)
+        {
+            Servings.Add(new Serving(dish));
+        }
+    }
+}
