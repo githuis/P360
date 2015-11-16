64b358d4ec2799f8d0df7104e7b0f6ed0369a941
Rasmus Møller Jensen <rjens14@student.aau.dk>
2015-11-16 14:44:31 +0100

    Add Dish class.

---
+++ Ordersystem/Model/Dish.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Dish
+    {
+        public Dish(string name, List<string> ingredients)
+        {
+            Name = name;
+            Ingredients = ingredients;
+        }
+
+        public string Name { get; set; }
+        public List<string> Ingredients { get; set; }
+        //public Image image { get; set; }
+        // TODO: Find ud af hvordan vi inkluderer billedet.
+
+        // TODO: Integrer med databaser.
+    }
+}
