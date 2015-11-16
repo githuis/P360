235883493717490a53865f6001c330d9f0ee7229
Rasmus Møller Jensen <rjens14@student.aau.dk>
2015-11-16 14:39:02 +0100

    Add diet property to Orderlist.

--- Ordersystem/Model/Orderlist.cs
+++ Ordersystem/Model/Orderlist.cs
@@ -8,16 +8,18 @@ namespace Model
 {
     public class Orderlist
     {
-        public Orderlist(List<DayMenu> dayMenus, DateTime startDate, DateTime endDate)
+        public Orderlist(List<DayMenu> dayMenus, DateTime startDate, DateTime endDate, string diet)
         {
             DayMenus = dayMenus;
             StartDate = startDate;
             EndDate = endDate;
+            Diet = diet;
         }
 
         public List<DayMenu> DayMenus { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public string Diet { get; set; }
 
         // TODO: Integrer med databaser.
     }
