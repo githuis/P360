fbc801e2257749a7cfe1ed330ce9539607d1bc38
Monzy <Magnushansen12@hotmail.com>
2015-11-16 15:24:54 +0100

    Add Serving class

---
+++ Ordersystem/Model/Serving.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Serving
+    {
+        public Serving(Dish dish)
+        {
+            Dish = dish;
+        }
+        public Dish Dish { get; private set; }
+    }
+}
