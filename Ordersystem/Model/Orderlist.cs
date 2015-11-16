625a254122d49477a2dc4c482a36c523c51a600a
Rasmus Møller Jensen <rjens14@student.aau.dk>
2015-11-16 14:29:04 +0100

    Add orderlist class

---
+++ Ordersystem/Model/Orderlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Orderlist
+    {
+        public Orderlist(List<DayMenu> dayMenus, DateTime startDate, DateTime endDate)
+        {
+            DayMenus = dayMenus;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public List<DayMenu> DayMenus { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        // TODO: Integrer med databaser.
+    }
+}
