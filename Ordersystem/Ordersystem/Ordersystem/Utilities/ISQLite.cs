2cdf0aa906594fe2ad5a8f1fdcb1ab6abc45ab26
Rasmus Møller Jensen <rjens14@student.aau.dk>
2015-11-20 11:35:04 +0100

    Implement shared local database.
    Still needs to implement platform specific variants.

--- Ordersystem/Ordersystem/Ordersystem/Utilities/ISQLite.cs
+++ Ordersystem/Ordersystem/Ordersystem/Utilities/ISQLite.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SQLite;
+﻿using SQLite;
 
 namespace Ordersystem.Utilities
 {
+    /// <summary>
+    /// The interface for the SQLite.
+    /// </summary>
     public interface ISQLite
     {
         SQLiteConnection GetConnection();
--- Ordersystem/Ordersystem/Ordersystem/Utilities/SQLItem.cs
+++ Ordersystem/Ordersystem/Ordersystem/Utilities/SQLItem.cs
@@ -1,20 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ordersystem.Model;
 using SQLite;
 
 namespace Ordersystem.Utilities
 {
-    public class SQLItem<T>
+    /// <summary>
+    /// Concatination of the information in the database.
+    /// Includes primary key property.
+    /// </summary>
+    public class SQLItem
     {
-        public SQLItem(T item)
+        /// <summary>
+        /// Empty constructor, needed for SQLite.
+        /// </summary>
+        public SQLItem()
         {
-            Item = item;
+            
         }
 
-        public T Item { get; private set; }
+        /// <summary>
+        /// Creates SQLItem with all relevant information.
+        /// </summary>
+        /// <param name="personNumber">The personNumber of the customer whom the order belongs to.</param>
+        /// <param name="order">The Order in the database.</param>
+        /// <param name="orderlist">The Orderlist relevant to the database.</param>
+        public SQLItem(int personNumber, Order order, Orderlist orderlist)
+        {
+            PersonNumber = personNumber;
+            Order = order;
+            Orderlist = orderlist;
+        }
+
+        /// <summary>
+        /// The personNumber of the customer whom the order belongs to.
+        /// </summary>
+        public int PersonNumber { get; private set; }
+
+        /// <summary>
+        /// The Order in the database.
+        /// </summary>
+        public Order Order { get; private set; }
+
+        /// <summary>
+        /// The Orderlist relevant to the database.
+        /// </summary>
+        public Orderlist Orderlist { get; private set; }
+
+        /// <summary>
+        /// The primary key ID needed to navigate the database.
+        /// </summary>
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
     }
