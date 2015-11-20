af48c281516fb780f7e112a0f6a3e784a3cf5328
Luraman <ponifi3d@gmail.com>
2015-11-20 10:18:45 +0100

    More broken

--- Ordersystem/Ordersystem/Ordersystem/Utilities/LocalDatabase.cs
+++ Ordersystem/Ordersystem/Ordersystem/Utilities/LocalDatabase.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 using SQLite;
+using Ordersystem.Model;
 
 namespace Ordersystem.Utilities
 {
@@ -13,6 +15,16 @@ namespace Ordersystem.Utilities
 
         public LocalDatabase()
         {
+            _database = DependencyService.Get<ISQLite>().GetConnection();
+            _database.CreateTable<SQLItem<Order>>();
+            _database.CreateTable<SQLItem<Orderlist>>();
+            _database.CreateTable<SQLItem<int>>();
+        }
+
+        public Order GetOrder(int value)
+        {
+            int id = _database.Table<SQLItem<int>>().FirstOrDefault(x => x.Item == value).ID;
+            return _database.Table<Order>().First(x => x. == id);
         }
     }
 }
---
+++ Ordersystem/Ordersystem/Ordersystem/Utilities/SQLItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Ordersystem.Utilities
+{
+    public class SQLItem<T>
+    {
+        public SQLItem(T item)
+        {
+            Item = item;
+        }
+
+        public T Item { get; private set; }
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+    }
+}
