ba990a07fb29ee9e51677e59866841a72f44f0fb
Monzy <Magnushansen12@hotmail.com>
2015-11-25 12:30:01 +0100

    Add DayMenuSelection

---
+++ Ordersystem/Ordersystem/Ordersystem/Model/DayMenuSelection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordersystem.Model
+{
+    public enum DayMenuChoice { Dish1, Dish2, NoDish }
+    public class DayMenuSelection
+    {
+        public DayMenuSelection(DayMenu dayMenu)
+        {
+            dayMenu = DayMenu;
+        }
+
+        public DayMenu DayMenu {get; private set;}
+        public DayMenuChoice Choice {get; set;}
+        public bool SideDish
+        {
+            get{ return SideDish; }
+            set
+            {
+                if (Choice != DayMenuChoice.NoDish)
+                    SideDish = true;
+            }
+        }
+
+        public List<Dish> GetDishes()
+        {
+            List<Dish> Dishes = new List<Dish>();
+            Dishes.Add(DayMenu.Dish1);
+            Dishes.Add(DayMenu.Dish2);
+            Dishes.Add(DayMenu.SideDish);
+            return Dishes;
+        }
+    }
+}
