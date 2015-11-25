5abc7b691a717cd8c03d8951d39c36e262ce45c1
GingerV1k1ng <thorhuga1@live.dk>
2015-11-25 08:11:07 +0100

    Trying brute force method of making GUI on the Main screen
    Made a error message when typing in a wrong social security number, for
    a bit of feed. Added 31 rows in the .axml file of Main screen. Trying
    to do it through brute force now.

--- Ordersystem/Ordersystem/Ordersystem.Droid/MainActivity.cs
+++ Ordersystem/Ordersystem/Ordersystem.Droid/MainActivity.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Ordersystem.Functions;
+using System.Collections.Generic;
 
 using Android.App;
 using Android.Content;
@@ -6,7 +8,6 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using Ordersystem.Functions;
 
 namespace Ordersystem.Droid
 {
@@ -26,13 +27,8 @@ namespace Ordersystem.Droid
 			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.myButton);
 			EditText editText = FindViewById<EditText> (Resource.Id.editText1);
-			TableRow tablerow1 = FindViewById<TableRow> (Resource.Id.row1);
-			TextView table_text2 = FindViewById<TextView> (Resource.Id.row_name2);
 
 			LogIn(button,editText);
-			tablerow1.Visibility = ViewStates.Invisible;
-			table_text2.Text = "test";
-
 		}
 
 		public void LogIn(Button button,EditText editText)
@@ -42,9 +38,95 @@ namespace Ordersystem.Droid
 				if(cm.ValidSocialSecurityNumber(editText.Text))
 				{
 					SetContentView (Resource.Layout.Main_Window);
+					CreateMainWindow ();
+				}
+				else
+				{
+					editText.Text = "Ikke et gyldigt log in, din so";	
 				}
 			};
 		}
+
+		public void CreateMainWindow()
+		{
+			MainPage = new ContentPage {
+				Content = new TableView {
+					Intent = TableIntent.Form,
+					Root = new TableRoot ("Table Title") {
+						new TableSection ("Section 1 Title") {
+							new TextCell {
+								Text = "TextCell Text",
+								Detail = "TextCell Detail"
+							},
+							new EntryCell {
+								Label = "EntryCell:",
+								Placeholder = "default keyboard",
+								Keyboard = Keyboard.Default
+							}
+						},
+						new TableSection ("Section 2 Title") {
+							new EntryCell {
+								Label = "Another EntryCell:",
+								Placeholder = "phone keyboard",
+								Keyboard = Keyboard.Telephone
+							},
+							new SwitchCell {
+								Text = "SwitchCell:"
+							}
+						}
+					}
+				}
+			}; 
+		}
+
+		/*public void CreateMainWindow()
+		{
+			//TableLayout Table1 = FindViewById<TableLayout> (Resource.Id.table);
+			//int i;
+
+			TableRow tablerow1 = FindViewById<TableRow> (Resource.Id.row1);
+			TableRow tablerow2 = FindViewById<TableRow> (Resource.Id.row2);
+			TableRow tablerow3 = FindViewById<TableRow> (Resource.Id.row3);
+			TableRow tablerow4 = FindViewById<TableRow> (Resource.Id.row4);
+			TableRow tablerow5 = FindViewById<TableRow> (Resource.Id.row5);
+			TableRow tablerow6 = FindViewById<TableRow> (Resource.Id.row6);
+			TableRow tablerow7 = FindViewById<TableRow> (Resource.Id.row7);
+			TableRow tablerow8 = FindViewById<TableRow> (Resource.Id.row8);
+			TableRow tablerow9 = FindViewById<TableRow> (Resource.Id.row9);
+			TableRow tablerow10 = FindViewById<TableRow> (Resource.Id.row10);
+			TableRow tablerow11 = FindViewById<TableRow> (Resource.Id.row11);
+			TableRow tablerow12 = FindViewById<TableRow> (Resource.Id.row12);
+			TableRow tablerow13 = FindViewById<TableRow> (Resource.Id.row13);
+			TableRow tablerow14 = FindViewById<TableRow> (Resource.Id.row14);
+			TableRow tablerow15 = FindViewById<TableRow> (Resource.Id.row15);
+			TableRow tablerow16 = FindViewById<TableRow> (Resource.Id.row16);
+			TableRow tablerow17 = FindViewById<TableRow> (Resource.Id.row17);
+			TableRow tablerow18 = FindViewById<TableRow> (Resource.Id.row18);
+			TableRow tablerow19 = FindViewById<TableRow> (Resource.Id.row19);
+			TableRow tablerow20 = FindViewById<TableRow> (Resource.Id.row20);
+			TableRow tablerow21 = FindViewById<TableRow> (Resource.Id.row21);
+			TableRow tablerow22 = FindViewById<TableRow> (Resource.Id.row22);
+			TableRow tablerow23 = FindViewById<TableRow> (Resource.Id.row23);
+			TableRow tablerow24 = FindViewById<TableRow> (Resource.Id.row24);
+			TableRow tablerow25 = FindViewById<TableRow> (Resource.Id.row25);
+			TableRow tablerow26 = FindViewById<TableRow> (Resource.Id.row26);
+			TableRow tablerow27 = FindViewById<TableRow> (Resource.Id.row27);
+			TableRow tablerow28 = FindViewById<TableRow> (Resource.Id.row28);
+			TableRow tablerow29 = FindViewById<TableRow> (Resource.Id.row29);
+			TableRow tablerow30 = FindViewById<TableRow> (Resource.Id.row30);
+			TableRow tablerow31 = FindViewById<TableRow> (Resource.Id.row31);
+
+		
+			for(i=1; i<=31;i++)
+			{
+			}
+
+
+			TextView table_text1 = FindViewById<TextView> (Resource.Id.row_name1);
+			Button table_button1 = FindViewById<Button> (Resource.Id.row_button1);
+			tablerow1.Visibility = ViewStates.Visible;
+			table_text1.Text = "test";	
+		}*/
 	}
 }
 
