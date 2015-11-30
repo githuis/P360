758348970790f44948ba66d2e34c3fa2d2435b6c
Rasmus Møller Jensen <rjens14@student.aau.dk>
2015-11-30 13:01:04 +0100

    Increase visibility of enums.

--- Ordersystem/Ordersystem/Ordersystem/Enums/Enumerations.cs
+++ Ordersystem/Ordersystem/Ordersystem/Enums/Enumerations.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Ordersystem.Enums
 {
-    public enum Diet { Full, LowFat, EnergyDense, SoftFoodsWPotatoes, SoftFoodsWMash } // Danish translation: Fuldkost, Hjertesund, Energitæt, Blødkost m/kartofler, Blødkost m/kartoffelmos
+    public enum Diet
+    {
+        Full, 
+        LowFat, 
+        EnergyDense, 
+        SoftFoodsWPotatoes, 
+        SoftFoodsWMash
+    } // Danish translation: Fuldkost, Hjertesund, Energitæt, Blødkost m/kartofler, Blødkost m/kartoffelmos
 
-    public enum DayMenuChoice { Dish1, Dish2, NoDish, NoChoice }
+    public enum DayMenuChoice
+    {
+        Dish1, 
+        Dish2, 
+        NoDish, 
+        NoChoice
+    }
 }
