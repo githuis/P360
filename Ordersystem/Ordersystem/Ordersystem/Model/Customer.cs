c66c237faa4c517b04afec900a5b0f6c6aa56472
Rasmus Møller Jensen <rjens14@student.aau.dk>
2015-11-30 10:26:12 +0100

    Add summaries for public members in customer.

--- Ordersystem/Ordersystem/Ordersystem/Model/Customer.cs
+++ Ordersystem/Ordersystem/Ordersystem/Model/Customer.cs
@@ -4,6 +4,12 @@ namespace Ordersystem.Model
 {
     public class Customer
     {
+        /// <summary>
+        /// A customer of Aalborg Madservice.
+        /// </summary>
+        /// <param name="personNumber">The social security number of the customer.</param>
+        /// <param name="name">The name of the customer.</param>
+        /// <param name="diet">The diet, the customer is getting.</param>
         public Customer(int personNumber, string name, Diet diet)
         {
             PersonNumber = personNumber;
@@ -12,9 +18,24 @@ namespace Ordersystem.Model
             Diet = diet;
         }
         
+        /// <summary>
+        /// The social security number of the customer.
+        /// </summary>
         public int PersonNumber { get; private set; }
+
+        /// <summary>
+        /// The name of the customer.
+        /// </summary>
         public string Name { get; private set; }
+
+        /// <summary>
+        /// The order of the customer for the next month.
+        /// </summary>
         public Order Order { get; set; }
+
+        /// <summary>
+        /// The diet, the customer is getting.
+        /// </summary>
         public Diet Diet { get; private set; }
     }
 }
