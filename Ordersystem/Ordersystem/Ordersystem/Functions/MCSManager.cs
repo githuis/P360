23a7ba664ac532165ba605c49ccf7bc1063c5a87
Monzy <Magnushansen12@hotmail.com>
2015-11-30 11:03:36 +0100

    Add namespaces

--- Ordersystem/Ordersystem/Ordersystem/Functions/MCSManager.cs
+++ Ordersystem/Ordersystem/Ordersystem/Functions/MCSManager.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ordersystem.Enums;
+using SQLite;
+using Ordersystem.Model;
+using Ordersystem.Utilities;
 
 namespace Ordersystem.Functions
 {
