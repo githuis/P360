d8882e35b813242752fdedca681a110e8b373213
Rasmus Møller Jensen <rjens14@student.aau.dk>
2015-11-30 10:33:10 +0100

    Add summaries to members in Dish.

--- Ordersystem/Ordersystem/Ordersystem/Model/Dish.cs
+++ Ordersystem/Ordersystem/Ordersystem/Model/Dish.cs
@@ -4,17 +4,28 @@ namespace Ordersystem.Model
 {
     public class Dish
     {
+        /// <summary>
+        /// A dish.
+        /// </summary>
+        /// <param name="name">The name of the dish.</param>
+        /// <param name="description">A description of the dish.</param>
         public Dish(string name, string description)
         {
             Name = name;
             Description = description;
         }
 
+        /// <summary>
+        /// The name of the dish.
+        /// </summary>
         public string Name { get; private set; }
+
+        /// <summary>
+        /// A description of the dish.
+        /// </summary>
         public string Description { get; private set; }
+
         //public Image image { get; set; }
         // TODO: Find ud af hvordan vi inkluderer billedet.
-
-        // TODO: Integrer med databaser.
     }
 }
