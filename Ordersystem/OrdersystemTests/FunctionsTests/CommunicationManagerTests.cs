151b83eaa300f0a98338d42e0c7be67dac6418ab
Monzy <Magnushansen12@hotmail.com>
2015-12-01 15:16:08 +0100

    Revert "Merge branch Funcion into Development"

--- Ordersystem/OrdersystemTests/FunctionsTests/CommunicationManagerTests.cs
+++ Ordersystem/OrdersystemTests/FunctionsTests/CommunicationManagerTests.cs
@@ -10,7 +10,7 @@ namespace OrdersystemTests.FunctionsTests
         [TestMethod]
         public void ValidSocialSecurityNumber_ValidInput_AssertTrue()
         {
-            LocalManager comMan = new LocalManager();
+            CommunicationManager comMan = new CommunicationManager();
 
             Assert.IsTrue(comMan.ValidSocialSecurityNumber("0706950435"));
         }
@@ -18,7 +18,7 @@ namespace OrdersystemTests.FunctionsTests
         [TestMethod]
         public void ValidSocialSecurityNumber_BorderInput_AssertTrue()
         {
-            LocalManager comMan = new LocalManager();
+            CommunicationManager comMan = new CommunicationManager();
 
             Assert.IsTrue(comMan.ValidSocialSecurityNumber("3112999999"));
         }
@@ -26,7 +26,7 @@ namespace OrdersystemTests.FunctionsTests
         [TestMethod]
         public void ValidSocialSecurityNumber_DayOverBorderImput_AsserFalse()
         {
-            LocalManager comMan = new LocalManager();
+            CommunicationManager comMan = new CommunicationManager();
 
             Assert.IsFalse(comMan.ValidSocialSecurityNumber("3212999999"));
         }
@@ -34,7 +34,7 @@ namespace OrdersystemTests.FunctionsTests
         [TestMethod]
         public void ValidSocialSecurityNumber_MonthOverBorderImput_AsserFalse()
         {
-            LocalManager comMan = new LocalManager();
+            CommunicationManager comMan = new CommunicationManager();
 
             Assert.IsFalse(comMan.ValidSocialSecurityNumber("3113999999"));
         }
@@ -42,7 +42,7 @@ namespace OrdersystemTests.FunctionsTests
         [TestMethod]
         public void ValidSocialSecurityNumber_VeryInvalidInput_AsserFalse()
         {
-            LocalManager comMan = new LocalManager();
+            CommunicationManager comMan = new CommunicationManager();
 
             Assert.IsFalse(comMan.ValidSocialSecurityNumber("9999999999"));
         }
@@ -50,7 +50,7 @@ namespace OrdersystemTests.FunctionsTests
         [TestMethod]
         public void ValidSocialSecurityNumber_ZeroInput_AssertFalse()
         {
-            LocalManager comMan = new LocalManager();
+            CommunicationManager comMan = new CommunicationManager();
 
             Assert.IsFalse(comMan.ValidSocialSecurityNumber("0000000000"));
         }
