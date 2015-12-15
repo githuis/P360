3dda7ecbae446d584179174b7dfee78233e34230
Rasmus Møller Jensen <rjens14@student.aau.dk>
2015-12-15 11:13:28 +0100

    Add model tests in test environment.

---
+++ Ordersystem/AndroidEvironmentTests/DayMenuTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using Ordersystem.Enums;
+using Ordersystem.Model;
+
+namespace AndroidEnvironmentTests
+{
+    [TestFixture]
+    public class DayMenuTests
+    {
+        [Test]
+        public void DayMenu_CreateWithValidInput_AssertTrue()
+        {
+            var dish1 = new Dish("Dish1", "This", "Here");
+            var dish2 = new Dish("Dish2", "That", "There");
+            var sdish = new Dish("SideDish", "Something", "Somewhere");
+            var dm = new DayMenu(dish1, dish2, sdish, new DateTime(2015, 11, 30));
+
+            Assert.IsTrue(dm.Dish1 == dish1 && dm.Dish2 == dish2 && dm.SideDish == sdish && dm.Date == new DateTime(2015, 11, 30));
+        }
+
+        [Test]
+        public void DayMenu_CreateWithNullDish1_AssertTrue()
+        {
+            var dish2 = new Dish("Dish2", "That", "There");
+            var sdish = new Dish("SideDish", "Something", "Somewhere");
+			Assert.Throws (typeof(ArgumentNullException), () => {
+				new DayMenu (null, dish2, sdish, new DateTime (2015, 11, 30));
+			});
+        }
+
+        [Test]
+        public void DayMenu_CreateWithNullDish2_AssertTrue()
+        {
+            var dish1 = new Dish("Dish1", "This", "Here");
+            var sdish = new Dish("SideDish", "Something", "Somewhere");
+			Assert.Throws (typeof(ArgumentNullException), () => {
+				new DayMenu (dish1, null, sdish, new DateTime (2015, 11, 30));
+			});
+        }
+
+        [Test]
+        public void DayMenu_CreateWithNullSideDish_AssertTrue()
+        {
+            var dish1 = new Dish("Dish1", "This", "Here");
+            var dish2 = new Dish("Dish2", "That", "There");
+			Assert.Throws (typeof(ArgumentNullException), () => {
+				new DayMenu (dish1, dish2, null, new DateTime (2015, 11, 30));
+			});
+        }
+    }
+}
---
+++ Ordersystem/AndroidEvironmentTests/OrderlistTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Ordersystem.Enums;
+using Ordersystem.Model;
+
+namespace AndroidEnvironmentTests
+{
+    [TestFixture]
+    public class OrderlistTests
+    {
+        [Test]
+        public void Orderlist_CreateWithValidInput_AssertTrue()
+        {
+
+            var list = new List<DayMenu>();
+            var orderlist = new Orderlist(list, new DateTime(2015, 11, 30), new DateTime(2015, 12, 30),
+                Diet.Full);
+            
+            Assert.IsTrue(list == orderlist.DayMenus && orderlist.StartDate == new DateTime(2015, 11, 30) &&
+				orderlist.EndDate == new DateTime(2015, 12, 30) && orderlist.Diet == Diet.Full);
+        }
+
+        [Test]
+        public void Orderlist_CreateWithNullList_AssertThrow()
+        {
+			Assert.Throws (typeof(ArgumentNullException), () => {
+				new Orderlist (null, new DateTime (2015, 11, 30), new DateTime (2015, 12, 30), Diet.Full);
+			});
+        }
+
+        [Test]
+        public void Orderlist_AddDayMenu_AssertTrue()
+        {
+            var orderlist = new Orderlist(new List<DayMenu>(), new DateTime(2015, 11, 30), new DateTime(2015, 12, 30),
+                Diet.Full);
+            var dayMenu = new DayMenu(new Dish("a", "a", "a"), new Dish("a", "a", "a"), new Dish("a", "a", "a"),
+                DateTime.Today);
+
+            orderlist.AddDayMenu(dayMenu);
+
+            Assert.IsTrue(orderlist.DayMenus.Contains(dayMenu));
+        }
+
+        [Test]
+        public void Orderlist_AddNullDayMenu_AssertThrow()
+        {
+            var orderlist = new Orderlist(new List<DayMenu>(), new DateTime(2015, 11, 30), new DateTime(2015, 12, 30),
+                Diet.Full);
+
+			Assert.Throws (typeof(ArgumentNullException), () => {
+				orderlist.AddDayMenu (null);
+			});
+
+            Assert.IsTrue(orderlist.DayMenus.Count == 0);
+        }
+
+        [Test]
+        public void Orderlist_GetDayMenuByDate_AssertTrue()
+        {
+            var orderlist = new Orderlist(new List<DayMenu>{new DayMenu(new Dish("a", "a", "a"), new Dish("a", "a", "a"), new Dish("a", "a", "a"),
+                DateTime.Today)}, new DateTime(2015, 11, 30), new DateTime(2015, 12, 30),
+                Diet.Full);
+
+            var dayMenu = orderlist.GetDayMenuByDate(DateTime.Today);
+
+            Assert.IsTrue(orderlist.DayMenus.Contains(dayMenu) && dayMenu.Date == DateTime.Today);
+        }
+
+        [Test]
+        public void Orderlist_GetDayMenuByWrongDate_AssertThrow()
+        {
+            var orderlist = new Orderlist(new List<DayMenu>{new DayMenu(new Dish("a", "a", "a"), new Dish("a", "a", "a"), new Dish("a", "a", "a"),
+                DateTime.Today)}, new DateTime(2015, 11, 30), new DateTime(2015, 12, 30),
+                Diet.Full);
+
+			Assert.Throws (typeof(NullReferenceException), () => {
+				orderlist.GetDayMenuByDate (new DateTime (1999, 1, 1));
+			});
+        }
+    }
+}
